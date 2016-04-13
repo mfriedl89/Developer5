7b3d25cdd8d86f9f80af28ee00fcba4bd58df3c4
Anika Jaindl <anika.jaindl@student.tugraz.at>
2016-04-13 16:39:32 +0200

    [AJ,RS] verification added

---
+++ Server/FindTutorialInCategory.php
@@ -0,0 +1,75 @@
+<?php
+/*	Reveives: title, category */
+/*  Returns Array: {[TutID,Title,Category,Difficulty,Duration]} */
+
+header('Content-type: application/json');
+
+
+if($_POST) {
+	$title   	= $_POST['title'];
+	$category   = $_POST['category'];
+
+	if($title && $category) {
+	
+			/* Load config file for connection */
+			$config = parse_ini_file("junk/config.ini");
+
+
+			$mysqli = new mysqli('sql04.sprit.org', $config['db_user'], $config['db_password'], $config['db_name']);
+
+			
+
+			/* check connection */
+			if (mysqli_connect_errno()) {
+				error_log("Connect failed: " . mysqli_connect_error());
+				echo '{"success":0,"error_message":"' . mysqli_connect_error() . '"}';
+			} else {
+			
+				$title = "%$title%";			
+				
+				if ($stmt = $mysqli->prepare("SELECT TutID,Title,Category,Difficulty,Duration FROM Tutorial WHERE Title LIKE ? AND Category = ?")) {
+				
+					$stmt->bind_param("ss",$title,$category);
+
+					/* execute query */
+					$stmt->execute();
+
+					/* bind result variables */
+					$stmt->bind_result($tID,$tTitle,$tCat,$tDif,$tDur);
+					
+					$foundTutorials = array();
+					
+					/* fetch values */
+					while ( $stmt->fetch() ){
+						$foundTutorials[] = $tID;
+						$foundTutorials[] = $tTitle;
+						$foundTutorials[] = $tCat;
+						$foundTutorials[] = $tDif;
+						$foundTutorials[] = $tDur;
+					}
+
+					/* close statement */
+					$stmt->close();
+				}
+	
+				/* close connection */
+				$mysqli->close();
+				
+				$json_encoded_output = json_encode($foundTutorials);
+				echo $json_encoded_output;
+				
+				
+			}
+	} else {
+		
+		//echo '{"success":-1,"error_message":"Invalid Arguments."}';
+		echo "Invalid Arguments.";
+	}
+
+}else {
+	
+	//echo '{"success":-1,"error_message":"Invalid Data."}';
+	echo "Invalid Data.";
+}
+
+?>
