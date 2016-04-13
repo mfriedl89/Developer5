47a09a87526267ea82fdafaaea2709365a7bd1ef
Markus Friedl <m.friedl@student.tugraz.at>
2016-04-13 09:48:35 +0200

    Add welcome/login screen

---
+++ Conari/Conari/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  Conari
+//
+//  Created by Markus Friedl on 13.04.16.
+//  Copyright © 2016 Markus Friedl. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func loginPressed(sender: UIButton) {
+    }
+
+    @IBAction func newUserPressed(sender: UIButton) {
+    }
+}
