3628c1867b9007c8eceffc8ea678ae0c0067e428
Paul Krassnig <paul.krassnig@aon.at>
2016-05-11 12:10:01 +0200

    added ChangeEmailViewController

---
+++ Conari/Conari/ChangeEmailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChangeEmailViewController.swift
+//  Conari
+//
+//  Created by Paul Krassnig on 11.05.16.
+//  Copyright © 2016 Markus Friedl. All rights reserved.
+//
+
+import UIKit
+
+class ChangeEmailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
