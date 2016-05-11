93db8feebfae4c4ff7a3e1500df3bc4396062c72
Paul Krassnig <paul.krassnig@aon.at>
2016-05-11 13:02:38 +0200

    added all new constrains, fixed constrains error

--- Conari/Conari/ChangeEmailViewController.swift
+++ Conari/Conari/ChangeEmailViewController.swift
@@ -10,74 +10,74 @@ import UIKit
 
 class ChangeEmailViewController: UIViewController {
   
-  @IBOutlet weak var old_email: UILabel!
-  @IBOutlet weak var new_email_textField: UITextField!
-  @IBOutlet weak var repeat_new_email_textField: UITextField!
-  @IBOutlet weak var Done_btn: UIButton!
-
-
-  let checkEmailFalse = -1
-  let repeatedEmailIsNotNew = -2
-  
-//  var login_password_ = DatabaseManager.sharedManager.getUserPassword()
-  var login_email_ = "irgendwos"
-  var newUserFunc = NewUserViewController()
-  
-
-  
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-  
-  func test (new_email:String, repeat_new_email:String) -> Bool {
- 
-    var error = 0
+//  @IBOutlet weak var old_email: UILabel!
+//  @IBOutlet weak var new_email_textField: UITextField!
+//  @IBOutlet weak var repeat_new_email_textField: UITextField!
+//  @IBOutlet weak var Done_btn: UIButton!
+//
+//
+//  let checkEmailFalse = -1
+//  let repeatedEmailIsNotNew = -2
+//  
+////  var login_password_ = DatabaseManager.sharedManager.getUserPassword()
+//  var login_email_ = "irgendwos"
+//  var newUserFunc = NewUserViewController()
+//  
+//
+//  
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//  
+//  func test (new_email:String, repeat_new_email:String) -> Bool {
+// 
+//    var error = 0
+//    
+//    if(!newUserFunc.checkEmailAddress(new_email)) {
+//      error = checkEmailFalse
+//    }
+//    else if(repeat_new_email != new_email) {
+//      error = repeatedEmailIsNotNew
+//    }
+//    
+//    
+//    newUserFunc.checkInput(newUserFunc.checkEmailAddress(new_email), textField: new_email_textField)
+//    newUserFunc.checkInput(repeat_new_email == new_email, textField: repeat_new_email_textField)
+//    
+//    switch error {
+//    case checkEmailFalse:
+//      let alert = UIAlertController(title: "Alert", message: "Please enter a valid Email address", preferredStyle: UIAlertControllerStyle.Alert)
+//      alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+//      self.presentViewController(alert, animated: true, completion: nil)
+//      break
+//    case repeatedEmailIsNotNew:
+//      let alert = UIAlertController(title: "Alert", message: "Repeated Email is not the same as New Email", preferredStyle: UIAlertControllerStyle.Alert)
+//      alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+//      self.presentViewController(alert, animated: true, completion: nil)
+//      break
+//    case 0:
+//      break
+//    default: break
+//    }
     
-    if(!newUserFunc.checkEmailAddress(new_email)) {
-      error = checkEmailFalse
-    }
-    else if(repeat_new_email != new_email) {
-      error = repeatedEmailIsNotNew
-    }
     
-    
-    newUserFunc.checkInput(newUserFunc.checkEmailAddress(new_email), textField: new_email_textField)
-    newUserFunc.checkInput(repeat_new_email == new_email, textField: repeat_new_email_textField)
-    
-    switch error {
-    case checkEmailFalse:
-      let alert = UIAlertController(title: "Alert", message: "Please enter a valid Email address", preferredStyle: UIAlertControllerStyle.Alert)
-      alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-      self.presentViewController(alert, animated: true, completion: nil)
-      break
-    case repeatedEmailIsNotNew:
-      let alert = UIAlertController(title: "Alert", message: "Repeated Email is not the same as New Email", preferredStyle: UIAlertControllerStyle.Alert)
-      alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-      self.presentViewController(alert, animated: true, completion: nil)
-      break
-    case 0:
-      break
-    default: break
-    }
-    
-    
-    return true
-  }
+//    return true
+//  }
   
   
   
-  
-  @IBAction func Done_button_clicked(sender: AnyObject) {
-    test(new_email_textField.text!, repeat_new_email: repeat_new_email_textField.text!)
-  
-  }
+//  
+//  @IBAction func Done_button_clicked(sender: AnyObject) {
+//    test(new_email_textField.text!, repeat_new_email: repeat_new_email_textField.text!)
+//
+//  }
 
     /*
     // MARK: - Navigation
