b4c4b60bcfc2fc0da534e9929891928a40d5684c
Wullschi <wullschleger@student.tugraz.at>
2016-05-11 11:37:57 +0200

    [SM,BW] fixed UITests for NewUserCreation in CategoryView merge

--- Conari/ConariUITests/NewUserUITests.swift
+++ Conari/ConariUITests/NewUserUITests.swift
@@ -28,7 +28,7 @@ class NewUserUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testAllUIElements() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
