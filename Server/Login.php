638d81f3ada31e5e34185006cdeaef7856297521
Markus Schofnegger <mschofnegger@student.tugraz.at>
2016-05-18 08:34:10 +0200

    [PK,MR] Updated Login method

--- Server/Login.php
+++ Server/Login.php
@@ -5,10 +5,12 @@
 
 header('Content-type: application/json');
 
+require 'utils.php';
+
 
 if($_POST) {
 	$username  = $_POST['username'];
-	$password = $_POST['password']; // is a string of stop names seperated by ';'
+	$password = $_POST['password'];
 	
 	if($username && $password) {
 
@@ -24,41 +26,15 @@ if($_POST) {
 				error_log("Connect failed: " . mysqli_connect_error());
 				echo '{"success":0,"error_message":"' . mysqli_connect_error() . '"}';
 			} else {
-				$UserID = 0;
-			
-				/* Check login*/
-				if ($stmt = $mysqli->prepare("SELECT UserID FROM USER WHERE Username = ? AND Password = ?")) {
+				$result = false;
 
-					// Build SHA512 value of password (bcrypt would be better, but this should be enough)
-					$password_hashed =  hash('sha512', $password);
-
-					/* bind parameters for markers */
-					$stmt->bind_param("ss", $username, $password_hashed);
-
-					/* execute query */
-					$stmt->execute();
-
-					/* bind result variables */
-					$stmt->bind_result($UserID);
-					
-					$stmt->fetch();
-					
-
-					/* close statement */
-					$stmt->close();
-				}
-
-					
-				if ($UserID) {
+				$result = validUser($mysqli, $username, $password);
+				if($result == true) {
 					echo "true";
-					/*error_log("Inserted requsted stations!");
-					*/
-					
-			
-				} else {
+				}
+				else {
 					error_log("Arguments do not match.");
 					echo '{"success":-1,"error_message":"We are sorry - This user does not exist!"}';
-					
 				}
 				
 			}
