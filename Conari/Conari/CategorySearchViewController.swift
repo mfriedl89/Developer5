2f80c5edb2efcece66aa419901d07eddead084b4
Stefan Mitterrutzner <mitterrutzner@student.tugraz.at>
2016-05-18 12:08:55 +0200

    [AJ, SM] added cocoapod and async image loading

--- Conari/Conari/CategorySearchViewController.swift
+++ Conari/Conari/CategorySearchViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import SDWebImage
 
-class CategorySearchViewController:UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UISearchDisplayDelegate, UISearchResultsUpdating  {
+class CategorySearchViewController:UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, SDWebImageManagerDelegate, UISearchDisplayDelegate, UISearchResultsUpdating  {
     
     var categories = ["All",
                       "Arts and Entertainment",
@@ -40,6 +41,9 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
     
     var searchController: UISearchController!
   
+  
+  
+  
     deinit{
       if let sc = searchController
       {
@@ -69,7 +73,6 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
         table_View.dataSource = self
         
         
-        
         screen_width = self.view.frame.width
         screen_height = self.view.frame.height
         
@@ -95,50 +98,54 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-      let cell = tableView.dequeueReusableCellWithIdentifier("SearchTableViewCell", forIndexPath: indexPath) as! CategorySearchTableViewCell
-    
-      if(indexPath.section == 0){
+      switch indexPath.section {
+      case 0:
+        let cell = tableView.dequeueReusableCellWithIdentifier("SearchTableViewCell", forIndexPath: indexPath) as! CategorySearchTableViewCell
         let duration_hours = Int(tutorial_array[indexPath.row].duration)!/60
         let duration_minutes = Int(tutorial_array[indexPath.row].duration)!%60
         
-        //cell.textLabel?.text = "test"
         cell.label_title?.text = tutorial_array[indexPath.row].title
         cell.label_category?.text = categories[tutorial_array[indexPath.row].category]
-      
+        
         cell.label_duration.text = String(format: "%02d:%02d", duration_hours,duration_minutes)
         cell.image_view.image = UIImage(named: "\(tutorial_array[indexPath.row].category-1)")
-      
+        
         switch Int(tutorial_array[indexPath.row].difficulty)! {
-          case 5:
-            cell.label_difficulty?.text = "very hard";
-            break;
-          case 4:
-            cell.label_difficulty?.text = "hard";
-            break;
-          case 3:
-            cell.label_difficulty?.text = "medium";
-            break;
-          case 2:
-            cell.label_difficulty?.text = "easy";
-            break;
-          case 1:
-            cell.label_difficulty?.text = "very easy";
-            break;
-          default:
-            break;
+        case 5:
+          cell.label_difficulty?.text = "very hard";
+          break;
+        case 4:
+          cell.label_difficulty?.text = "hard";
+          break;
+        case 3:
+          cell.label_difficulty?.text = "medium";
+          break;
+        case 2:
+          cell.label_difficulty?.text = "easy";
+          break;
+        case 1:
+          cell.label_difficulty?.text = "very easy";
+          break;
+        default:
+          break
         }
-      }else if(indexPath.section == 1)
-      {
+        return cell
+      case 1:
+        let cell = tableView.dequeueReusableCellWithIdentifier("SearchTableYoutubeViewCell", forIndexPath: indexPath) as! CategorySearchYoutubeTableViewCell
         cell.label_title?.text = youtube_array[indexPath.row].title
-        cell.label_category?.text = ""
-        cell.label_duration?.text = ""
-        
-        //loadImageFromUrl(self.youtube_array[indexPath.row].thumbnail,view: cell.imageView!)
-        
-        
+                cell.imageView?.sd_setImageWithURL(NSURL(string:self.youtube_array[indexPath.row].thumbnail), placeholderImage: UIImage(), completed: {(image: UIImage?, error: NSError?, cacheType: SDImageCacheType!, imageURL: NSURL?) in
+          if let cellToUpdate = self.table_View?.cellForRowAtIndexPath(indexPath)
+          {
+            cellToUpdate.setNeedsLayout()
+          }
+        })
+        return cell
 
+      default: break
+        
       }
-      return cell
+      return UITableViewCell()
+
     }
     
     func tableView(tableView:UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -158,15 +165,25 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
     
     func tableView(tableView:UITableView, heightForRowAtIndexPath indexPath:NSIndexPath)->CGFloat
     {
-        return 70
+        return 100
     }
   
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
       switch section {
       case 0:
-        return "Tutorials"
+        if tutorial_array.count == 0 {
+          return ""
+        }else
+        {
+          return "Tutorials"
+        }
       case 1:
-        return "Youtube"
+        if youtube_array.count == 0 {
+          return ""
+        }else
+        {
+          return "Youtube"
+        }
       default:
         return ""
       }
@@ -175,9 +192,22 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
   
   
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+      switch indexPath.section {
+      case 0:
         self.performSegueWithIdentifier("show_tutorial", sender: indexPath.row)
-        tableView.deselectRowAtIndexPath(indexPath, animated: false);
+        break;
+      case 1:
+        break;
+      default:
+        return
+      }
+      tableView.deselectRowAtIndexPath(indexPath, animated: false);
     }
+  
+  func tableView(tableView: UITableView, didEndDisplayingCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+    cell.imageView?.sd_cancelCurrentImageLoad();
+    cell.imageView?.image = nil
+  }
     
     
     
@@ -305,45 +335,10 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
     
     }
   
-    //source: https://softwarejuancarlos.com/2015/11/27/swift-2-examples-loading-an-image-from-a-url/
-    func loadImageFromUrl(url: String, view: UIImageView){
-      
-      // Create Url from string
-      let url = NSURL(string: url)!
-      
-      // Download task:
-      // - sharedSession = global NSURLCache, NSHTTPCookieStorage and NSURLCredentialStorage objects.
-      let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (responseData, responseUrl, error) -> Void in
-        // if responseData is not null...
-        if let data = responseData{
-          
-          // execute in UI thread
-          let image = self.scaleImage(UIImage(data: data)!, toSize: CGSize(width: 65, height:65))
-          
-          dispatch_async(dispatch_get_main_queue(), { () -> Void in
-            view.image = image
-            self.table_View.reloadData()
-          })
-        }
-      }
-      
-      // Run task
-      task.resume()
-    }
+
   
-    //source: http://stackoverflow.com/a/34599236
-    func scaleImage(image: UIImage, toSize newSize: CGSize) -> (UIImage) {
-      let newRect = CGRectIntegral(CGRectMake(0,0, newSize.width, newSize.height))
-      UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
-      let context = UIGraphicsGetCurrentContext()
-      CGContextSetInterpolationQuality(context, .High)
-      let flipVertical = CGAffineTransformMake(1, 0, 0, -1, 0, newSize.height)
-      CGContextConcatCTM(context, flipVertical)
-      CGContextDrawImage(context, newRect, image.CGImage)
-      let newImage = UIImage(CGImage: CGBitmapContextCreateImage(context)!)
-      UIGraphicsEndImageContext()
-      return newImage
-    }
+
+
   
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "show_tutorial"
