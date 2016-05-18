d8f27ab325f7992b6509ee79e1328a121d2ff464
Stefan Mitterrutzner <mitterrutzner@student.tugraz.at>
2016-05-18 10:31:00 +0200

    [AJ, SM] added Youtube Manager and integrated youtube result section

--- Conari/Conari/CategorySearchViewController.swift
+++ Conari/Conari/CategorySearchViewController.swift
@@ -52,6 +52,7 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
   
     
     var tutorial_array = [Tutorial_item]()
+    var youtube_array = [YoutubeVideo]()
     
         
     @IBOutlet var table_View: UITableView!
@@ -60,80 +61,53 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
         self.navigationController?.navigationBarHidden = false
     }
     
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
-            
-            table_View.delegate = self
-            table_View.dataSource = self
-            
-            
-            
-            screen_width = self.view.frame.width
-            screen_height = self.view.frame.height
-            
-            
-            if (text_search == "")
-            {
-                configureSearchController()
-                self.title = categories[selected_category]
-            }else
-            {
-                self.title = text_search
-            
-            }
-            
-            
-            
-            
-            DatabaseManager.sharedManager.findTutorialByCategory(text_search, tutorial_category: selected_category) { (response) in
-                
-                if(!response.isEmpty){
-                    
-                    self.tutorial_array = response
-                    
-                    self.table_View.performSelectorOnMainThread(#selector(UITableView.reloadData), withObject: nil, waitUntilDone: true)
-                    //self.table_View.reloadData()
-                    
-                    print("Tutorial Count: \(response.count)")
-                    
-                }else{
-                    
-                    print("Tutorial not found")
-                    self.tutorial_array.removeAll()
-                    self.table_View.performSelectorOnMainThread(#selector(UITableView.reloadData), withObject: nil, waitUntilDone: true)
-                    print("Tutorial Count: \(self.tutorial_array.count)")
-                    //self.table_View.reloadData()
-                }
-                
-              dispatch_async(dispatch_get_main_queue(),
-                             {
-                              self.table_View.reloadData();
-              })
-                
-            }
-
-        }
-    
-    
-    
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
+      override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        table_View.delegate = self
+        table_View.dataSource = self
+        
+        
+        
+        screen_width = self.view.frame.width
+        screen_height = self.view.frame.height
+        
+        
+        if (text_search == "")
+        {
+            configureSearchController()
+            self.title = categories[selected_category]
+        }else
+        {
+            self.title = text_search
+        
         }
         
-        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-            let cell = tableView.dequeueReusableCellWithIdentifier("SearchTableViewCell", forIndexPath: indexPath) as! CategorySearchTableViewCell
-            let duration_hours = Int(tutorial_array[indexPath.row].duration)!/60
-            let duration_minutes = Int(tutorial_array[indexPath.row].duration)!%60
-            
-            //cell.textLabel?.text = "test"
-            cell.label_title?.text = tutorial_array[indexPath.row].title
-            cell.label_category?.text = categories[tutorial_array[indexPath.row].category]
-          
-            cell.label_duration.text = String(format: "%02d:%02d", duration_hours,duration_minutes)
-            cell.image_view.image = UIImage(named: "\(tutorial_array[indexPath.row].category-1)")
-          
+        reloadArrays()
+    }
+
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+      let cell = tableView.dequeueReusableCellWithIdentifier("SearchTableViewCell", forIndexPath: indexPath) as! CategorySearchTableViewCell
+    
+      if(indexPath.section == 0){
+        let duration_hours = Int(tutorial_array[indexPath.row].duration)!/60
+        let duration_minutes = Int(tutorial_array[indexPath.row].duration)!%60
+        
+        //cell.textLabel?.text = "test"
+        cell.label_title?.text = tutorial_array[indexPath.row].title
+        cell.label_category?.text = categories[tutorial_array[indexPath.row].category]
+      
+        cell.label_duration.text = String(format: "%02d:%02d", duration_hours,duration_minutes)
+        cell.image_view.image = UIImage(named: "\(tutorial_array[indexPath.row].category-1)")
+      
         switch Int(tutorial_array[indexPath.row].difficulty)! {
           case 5:
             cell.label_difficulty?.text = "very hard";
@@ -152,28 +126,54 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
             break;
           default:
             break;
-          }
+        }
+      }else if(indexPath.section == 1)
+      {
+        cell.label_title?.text = youtube_array[indexPath.row].title
+        cell.label_category?.text = ""
+        cell.label_duration?.text = ""
+        
+        //loadImageFromUrl(self.youtube_array[indexPath.row].thumbnail,view: cell.imageView!)
+        
+        
 
-            
-            return cell
-        }
-        
-        func tableView(tableView:UITableView, numberOfRowsInSection section: Int) -> Int {
-            
-            return tutorial_array.count
-        }
-        
-        func numberOfSectionsInTableView(tableView:UITableView) -> Int {
-            
-            return 1
-        }
-        
-        func tableView(tableView:UITableView, heightForRowAtIndexPath indexPath:NSIndexPath)->CGFloat
-        {
-            return 70
-        }
+      }
+      return cell
+    }
     
+    func tableView(tableView:UITableView, numberOfRowsInSection section: Int) -> Int {
+      switch section {
+      case 0:
+        return tutorial_array.count
+      case 1:
+        return youtube_array.count
+      default:
+        return 0
+      }
+    }
+  
+    func numberOfSectionsInTableView(tableView:UITableView) -> Int {
+      return 2
+    }
     
+    func tableView(tableView:UITableView, heightForRowAtIndexPath indexPath:NSIndexPath)->CGFloat
+    {
+        return 70
+    }
+  
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+      switch section {
+      case 0:
+        return "Tutorials"
+      case 1:
+        return "Youtube"
+      default:
+        return ""
+      }
+    }
+  
+  
+  
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         self.performSegueWithIdentifier("show_tutorial", sender: indexPath.row)
         tableView.deselectRowAtIndexPath(indexPath, animated: false);
@@ -182,19 +182,19 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
     
     
     func configureSearchController() {
-        // Initialize and perform a minimum configuration to the search controller.
-        searchController = UISearchController(searchResultsController: nil)
-        searchController.searchResultsUpdater = self
-        searchController.dimsBackgroundDuringPresentation = true
-        searchController.searchBar.placeholder = "Search here..."
-        searchController.searchBar.delegate = self
-        searchController.searchBar.sizeToFit()
-        //searchController.searchBar.frame = CGRect(x: 0, y: 0, width: screen_width, height: 50)
-        
-        // Place the search bar view to the tableview headerview.
-        table_View.tableHeaderView = searchController.searchBar
-        //self.search_bar_view.addSubview(searchController.searchBar)
-        //self.search_bar_view.bringSubviewToFront(searchController.searchBar)
+      // Initialize and perform a minimum configuration to the search controller.
+      searchController = UISearchController(searchResultsController: nil)
+      searchController.searchResultsUpdater = self
+      searchController.dimsBackgroundDuringPresentation = true
+      searchController.searchBar.placeholder = "Search here..."
+      searchController.searchBar.delegate = self
+      searchController.searchBar.sizeToFit()
+      //searchController.searchBar.frame = CGRect(x: 0, y: 0, width: screen_width, height: 50)
+      
+      // Place the search bar view to the tableview headerview.
+      table_View.tableHeaderView = searchController.searchBar
+      //self.search_bar_view.addSubview(searchController.searchBar)
+      //self.search_bar_view.bringSubviewToFront(searchController.searchBar)
     }
   
   
@@ -211,35 +211,7 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
       {
         searchBar.placeholder = "Search here..."
       }
-      DatabaseManager.sharedManager.findTutorialByCategory(text_search, tutorial_category: selected_category) { (response) in
-        
-        if(!response.isEmpty){
-          
-          self.tutorial_array = response
-          
-          self.table_View.performSelectorOnMainThread(#selector(UITableView.reloadData), withObject: nil, waitUntilDone: true)
-          //self.table_View.reloadData()
-          
-          print("Tutorial Count: \(response.count)")
-          
-        }else{
-          
-          print("Tutorial not found")
-          self.tutorial_array.removeAll()
-          self.table_View.performSelectorOnMainThread(#selector(UITableView.reloadData), withObject: nil, waitUntilDone: true)
-          print("Tutorial Count: \(self.tutorial_array.count)")
-          //self.table_View.reloadData()
-        }
-        
-        // Reload the tableview.
-        //self.tableView.reloadData()
-        
-        dispatch_async(dispatch_get_main_queue(),
-                       {
-                        self.table_View.reloadData();
-        })
-        
-      }
+      reloadArrays()
     }
 
     
@@ -262,36 +234,7 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
         searchBar.placeholder = "Search here..."
       }
       
-      DatabaseManager.sharedManager.findTutorialByCategory(text_search, tutorial_category: selected_category) { (response) in
-        
-        if(!response.isEmpty){
-          
-          self.tutorial_array = response
-          
-          self.table_View.performSelectorOnMainThread(#selector(UITableView.reloadData), withObject: nil, waitUntilDone: true)
-          //self.table_View.reloadData()
-          
-          print("Tutorial Count: \(response.count)")
-          
-        }else{
-          
-          print("Tutorial not found")
-          self.tutorial_array.removeAll()
-          self.table_View.performSelectorOnMainThread(#selector(UITableView.reloadData), withObject: nil, waitUntilDone: true)
-          print("Tutorial Count: \(self.tutorial_array.count)")
-          //self.table_View.reloadData()
-        }
-        
-        // Reload the tableview.
-        //self.tableView.reloadData()
-        
-        dispatch_async(dispatch_get_main_queue(),
-                       {
-                        self.table_View.reloadData();
-        })
-        
-      }
-      
+      reloadArrays()
     }
   
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
@@ -312,41 +255,96 @@ class CategorySearchViewController:UIViewController, UITableViewDelegate, UITabl
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
         let searchString = searchController.searchBar.text
         text_search = searchString!
-
-      
-
-
-        //if(searchString == "")
-        //{
-            DatabaseManager.sharedManager.findTutorialByCategory(searchString!, tutorial_category: selected_category) { (response) in
-                
-                if(!response.isEmpty){
-                    
-                    self.tutorial_array = response
-                    
-                    self.table_View.performSelectorOnMainThread(#selector(UITableView.reloadData), withObject: nil, waitUntilDone: true)
-                    //self.table_View.reloadData()
-                    
-                    print("Tutorial Count: \(response.count)")
-                    
-                }else{
-                    
-                    print("Tutorial not found")
-                    self.tutorial_array.removeAll()
-                    self.table_View.performSelectorOnMainThread(#selector(UITableView.reloadData), withObject: nil, waitUntilDone: true)
-                    print("Tutorial Count: \(self.tutorial_array.count)")
-                    //self.table_View.reloadData()
-                }
-                
-              dispatch_async(dispatch_get_main_queue(),
-                             {
-                              self.table_View.reloadData();
-              })
-                
-            }
-        //}
+        reloadArrays()
     }
+  
+    func reloadArrays()
+    {
+      YoutubeManager.sharedManager.searchVideoByTitle(text_search, completionHandler: {
+        (response) in
+        if(response.isEmpty)
+        {
+          return
+        }
+        self.youtube_array.removeAll()
+        self.youtube_array = response
+        dispatch_async(dispatch_get_main_queue(),
+          {
+            self.table_View.reloadData();
+        })
+      })
+      
+      DatabaseManager.sharedManager.findTutorialByCategory(text_search, tutorial_category: selected_category) { (response) in
+        
+        if(!response.isEmpty){
+          
+          self.tutorial_array = response
+          
+          self.table_View.performSelectorOnMainThread(#selector(UITableView.reloadData), withObject: nil, waitUntilDone: true)
+          //self.table_View.reloadData()
+          
+          print("Tutorial Count: \(response.count)")
+          
+        }else{
+          
+          print("Tutorial not found")
+          self.tutorial_array.removeAll()
+          self.table_View.performSelectorOnMainThread(#selector(UITableView.reloadData), withObject: nil, waitUntilDone: true)
+          print("Tutorial Count: \(self.tutorial_array.count)")
+          //self.table_View.reloadData()
+        }
+        
+        dispatch_async(dispatch_get_main_queue(),
+                       {
+                        self.table_View.reloadData();
+        })
+        
+      }
+
+
     
+    }
+  
+    //source: https://softwarejuancarlos.com/2015/11/27/swift-2-examples-loading-an-image-from-a-url/
+    func loadImageFromUrl(url: String, view: UIImageView){
+      
+      // Create Url from string
+      let url = NSURL(string: url)!
+      
+      // Download task:
+      // - sharedSession = global NSURLCache, NSHTTPCookieStorage and NSURLCredentialStorage objects.
+      let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (responseData, responseUrl, error) -> Void in
+        // if responseData is not null...
+        if let data = responseData{
+          
+          // execute in UI thread
+          let image = self.scaleImage(UIImage(data: data)!, toSize: CGSize(width: 65, height:65))
+          
+          dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            view.image = image
+            self.table_View.reloadData()
+          })
+        }
+      }
+      
+      // Run task
+      task.resume()
+    }
+  
+    //source: http://stackoverflow.com/a/34599236
+    func scaleImage(image: UIImage, toSize newSize: CGSize) -> (UIImage) {
+      let newRect = CGRectIntegral(CGRectMake(0,0, newSize.width, newSize.height))
+      UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+      let context = UIGraphicsGetCurrentContext()
+      CGContextSetInterpolationQuality(context, .High)
+      let flipVertical = CGAffineTransformMake(1, 0, 0, -1, 0, newSize.height)
+      CGContextConcatCTM(context, flipVertical)
+      CGContextDrawImage(context, newRect, image.CGImage)
+      let newImage = UIImage(CGImage: CGBitmapContextCreateImage(context)!)
+      UIGraphicsEndImageContext()
+      return newImage
+    }
+  
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "show_tutorial"
         {
---
+++ Conari/Conari/YoutubeManager.swift
@@ -0,0 +1,84 @@
+//
+//  YoutubeManager.swift
+//  Conari
+//
+//  Created by Stefan Mitterrutzner on 18/05/16.
+//  Copyright © 2016 Markus Friedl. All rights reserved.
+//
+
+import Foundation
+
+struct YoutubeVideo {
+  var title: String
+  var thumbnail:String
+  var videoId: String
+}
+
+
+/**
+ This file will act as our Youutube manager.
+ */
+class YoutubeManager {
+  
+  /** Singletone instance. */
+  static let sharedManager = YoutubeManager()
+  let apiKey = "AIzaSyBkPodgj3cSzd8XYnzEnUBIsonzRx7QaZA"
+
+  
+  func searchVideoByTitle(title: String, completionHandler: (response: [YoutubeVideo]) -> Void) -> Void {
+    
+    var urlString = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=\(title)&type=video&key=\(apiKey)"
+    //urlString = urlString.stringByAddingPercentEncodingWithAllowedCharacters()!
+    
+    // Create a NSURL object based on the above string.
+    let targetURL = NSURL(string: urlString)
+    let request = NSMutableURLRequest(URL: targetURL!)
+    
+    let task = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: {data, response, error in
+      guard error == nil && data != nil else {
+        // check for fundamental networking error
+        print("error=\(error)")
+        return
+      }
+      
+      if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 200 {
+        // check for http errors
+        print("statusCode should be 200, but is \(httpStatus.statusCode)")
+        print("response = \(response)")
+        return
+      }
+      
+      do {
+        let resultsDict = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as! Dictionary<NSObject, AnyObject>
+        
+        var returnArray = [YoutubeVideo]()
+        
+        // Get all search result items ("items" array).
+        let items: Array<Dictionary<NSObject, AnyObject>> = resultsDict["items"] as! Array<Dictionary<NSObject, AnyObject>>
+        for item in items
+        {
+          let snippetDict = item["snippet"] as! Dictionary<NSObject, AnyObject>
+          
+           let title = snippetDict["title"] as! String
+           let thumbnail = ((snippetDict["thumbnails"] as! Dictionary<NSObject, AnyObject>)["default"] as! Dictionary<NSObject, AnyObject>)["url"] as! String
+           let videoid = (item["id"] as! Dictionary<NSObject, AnyObject>)["videoId"] as! String
+            
+          returnArray.append(YoutubeVideo(title: title,thumbnail: thumbnail,videoId: videoid))
+        
+        }
+        completionHandler(response: returnArray)
+
+        
+        
+        
+      } catch {
+        print("error serializing JSON: \(error)")
+      }
+      
+    })
+    task.resume()
+
+    
+  }
+  
+}
