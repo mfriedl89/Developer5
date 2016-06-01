fae20af8ec3ebeb0b7e7aeb64517f2f9829cabd4
Benjamin Wullschleger <wullschleger@student.tugraz.at>
2016-06-01 14:49:22 +0200

    [BW] Fixed a UI Test, and added another one in MetaDataUITests

--- Conari/ConariUITests/MetadataUITests.swift
+++ Conari/ConariUITests/MetadataUITests.swift
@@ -21,10 +21,8 @@ class MetadataUITests: XCTestCase {
     super.tearDown()
   }
   
-  func testExample() {
-    // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-    
+  func testTextTutorial() {
+   
     sleep(1);
     
     let app = XCUIApplication()
@@ -92,14 +90,68 @@ class MetadataUITests: XCTestCase {
     returnButton.tap()
     sleep(1);
     element2.childrenMatchingType(.TextField).element
-    app.toolbars.containingType(.Button, identifier:"Ordered List").childrenMatchingType(.Button).elementBoundByIndex(6).tap()
+    app.toolbars.containingType(.Button, identifier:"▽").childrenMatchingType(.Button).elementBoundByIndex(6).tap()
     sleep(1);
     app.sheets.collectionViews.buttons["Photo Library"].tap()
     app.tables.buttons["Moments"].tap()
     app.collectionViews["PhotosGridView"].cells["Photo, Landscape, March 13, 2011, 1:17 AM"].tap()
     sleep(1)
     app.navigationBars["test"].buttons["Save"].tap()
-    //app.navigationBars["Test"].buttons["Save"].tap()
+  }
+  
+  
+  func testVideoTutorial() {
+    
+    sleep(1);
+    
+    let app = XCUIApplication()
+    let answerButton = app.buttons["Login"]
+    answerButton.tap()
+    
+    sleep(1);
+    
+    XCTAssert(app.staticTexts["Username:"].exists)
+    XCTAssert(app.staticTexts["Password:"].exists)
+    
+    let textFieldUsername = app.textFields["username"]
+    let textFieldPassword = app.secureTextFields["password"]
+    
+    textFieldUsername.tap()
+    XCTAssertTrue(textFieldUsername.exists, "Text field username doesn't exist")
+    textFieldUsername.typeText("anton")
+    XCTAssertEqual(textFieldUsername.value as? String, "anton")
+    
+    textFieldPassword.tap()
+    XCTAssertTrue(textFieldPassword.exists, "Text field password doesn't exist")
+    textFieldPassword.typeText("Test1234@")
+    
+    app.buttons["Login"].tap()
+    
+    sleep(1);
+    
+    app.buttons["Create Video Tutorial"].tap()
+    
+    sleep(1);
+    
+    app.textFields["title"].tap()
+    app.textFields["title"].typeText("test")
+    XCTAssertEqual(app.textFields["title"].value as? String, "test")
+    app.buttons["Done"].tap()
+    
+    sleep(1);
+    
+    let incrementButton = app.steppers.buttons["Increment"]
+    incrementButton.tap()
+    incrementButton.tap()
+    
+    app.textFields["category"].tap()
+    app.pickerWheels["Arts and Entertainment"].adjustToPickerWheelValue("Conari")
+    app.textFields["duration"].tap()
+    app.pickerWheels["00:00"].adjustToPickerWheelValue("00:10")
+    app.buttons["Select Video"].tap()
+    app.sheets.collectionViews.buttons["Photo Library"].tap()
+    app.navigationBars["Photos"].buttons["Cancel"].tap()
+    
     
   }
   
