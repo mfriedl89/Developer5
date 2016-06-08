de0b1859db370dcb628b52af68acd248eebaeb69
Paul Krassnig <paul.krassnig@aon.at>
2016-06-08 09:32:50 +0200

    [PK / MF] changed background color of views and textcolors

--- Conari/Conari/Constants.swift
+++ Conari/Conari/Constants.swift
@@ -17,15 +17,21 @@ class Constants {
   // MARK: - Appearance Colors
   
   static let navigationBarBackgroundColor = UIColor(red: 0.25, green: 0.04, blue: 0.40, alpha: 1.00)
+
   static let navigationBarTextColor = UIColor.whiteColor()
   
   //static let buttonColor = UIColor(red: 0.00, green: 0.90, blue: 0.69, alpha: 1.00)
-  static let buttonColor = UIColor.yellowColor()
-  static let labelColor = UIColor.whiteColor()
+//  static let buttonColor = UIColor.yellowColor()
+  static let buttonColor = UIColor(red:0.57, green:0.10, blue:0.75, alpha:1.0)
+
+//  static let labelColor = UIColor.whiteColor()
+  static let labelColor = UIColor.blackColor()
+
   
-  static let viewBackgroundColor = UIColor(red:0.49, green:0.00, blue:0.78, alpha:1.00)
+//  static let viewBackgroundColor = UIColor(red:0.49, green:0.00, blue:0.78, alpha:1.00)
+  static let viewBackgroundColor = UIColor(red:1, green:1, blue:1, alpha:1)
   static let contentBackgroundColor = UIColor(red: 0.45, green: 0.24, blue: 0.81, alpha: 1.00)
   
-  static let cssTextColor = "<style>body{color: white;}</style>"
-  
+//  static let cssTextColor = "<style>body{color: white;}</style>"
+  static let cssTextColor = "<style>body{color: black;}</style>"
 }
--- Conari/Conari/WelcomeScreenViewController.swift
+++ Conari/Conari/WelcomeScreenViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class WelcomeScreenViewController: UIViewController {
   
+  @IBOutlet weak var WelcomeLabel: UILabel!
+  @IBOutlet weak var MrTutorLabel: UILabel!
+  @IBOutlet weak var LoginBtn: UIButton!
+  @IBOutlet weak var ContinueBtn: UIButton!
+  @IBOutlet weak var AboutBtn: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.view.backgroundColor = Constants.viewBackgroundColor
+    
+    self.view.backgroundColor = UIColor(red: 0.25, green: 0.04, blue: 0.40, alpha: 1.00)
+    
+    self.WelcomeLabel.textColor = UIColor.whiteColor()
+    self.MrTutorLabel.textColor = UIColor.whiteColor()
+    
+    self.LoginBtn.backgroundColor = UIColor.whiteColor()
+    self.ContinueBtn.backgroundColor = UIColor.whiteColor()
+    
+    self.LoginBtn.tintColor = UIColor.blackColor()
+    self.ContinueBtn.tintColor = UIColor.blackColor()
+    self.AboutBtn.tintColor = UIColor.whiteColor()
+    
+    let cornerRadius : CGFloat = 15
+    let borderWith : CGFloat = 1
+    let borderColor = UIColor.whiteColor().CGColor
+
+    self.LoginBtn.layer.cornerRadius = cornerRadius
+    self.LoginBtn.layer.borderWidth = borderWith
+    self.LoginBtn.layer.borderColor = borderColor
+    
+    self.ContinueBtn.layer.cornerRadius = cornerRadius
+    self.ContinueBtn.layer.borderWidth = borderWith
+    self.ContinueBtn.layer.borderColor = borderColor
+
   }
   
   override func didReceiveMemoryWarning() {
