f988dd5dc37a4a515f030b4a8786b8b360bdae38
Paul Krassnig <paul.krassnig@aon.at>
2016-06-08 14:15:26 +0200

    [PK / MF] changed menu view and font style to light

--- Conari/Conari/MenuViewController.swift
+++ Conari/Conari/MenuViewController.swift
@@ -12,6 +12,16 @@ class MenuViewController: UIViewController {
   
   @IBOutlet weak var userNameLabel: UILabel!
   
+  @IBOutlet weak var SearchTutBtn: UIButton!
+  @IBOutlet weak var CreateTutBtn: UIButton!
+  @IBOutlet weak var CreateVidTut: UIButton!
+  @IBOutlet weak var ManageTut: UIButton!
+  
+  @IBOutlet weak var ChangeNameBtn: UIButton!
+  @IBOutlet weak var ChangeEmailBtn: UIButton!
+  @IBOutlet weak var ChangePasswordBtn: UIButton!
+  @IBOutlet weak var LogoutBtn: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     self.view.backgroundColor = Constants.viewBackgroundColor
@@ -21,12 +31,37 @@ class MenuViewController: UIViewController {
     userNameLabel.text = DatabaseManager.sharedManager.getUserName()
     
     // Do any additional setup after loading the view.
+    
+    Constants.setGradientColor(self.view)
+    
+    Constants.setRadiusWithColor(UIColor.whiteColor(), forButton: SearchTutBtn)
+    Constants.setRadiusWithColor(UIColor.whiteColor(), forButton: CreateTutBtn)
+    Constants.setRadiusWithColor(UIColor.whiteColor(), forButton: CreateVidTut)
+    Constants.setRadiusWithColor(UIColor.whiteColor(), forButton: ManageTut)
+
+    
+    self.userNameLabel.tintColor = UIColor.whiteColor()
+    self.SearchTutBtn.tintColor = UIColor.whiteColor()
+    self.CreateTutBtn.tintColor = UIColor.whiteColor()
+    self.CreateVidTut.tintColor = UIColor.whiteColor()
+    self.ManageTut.tintColor = UIColor.whiteColor()
+    self.ChangeNameBtn.tintColor = UIColor.whiteColor()
+    self.ChangeEmailBtn.tintColor = UIColor.whiteColor()
+    self.ChangePasswordBtn.tintColor = UIColor.whiteColor()
+    self.LogoutBtn.tintColor = UIColor.whiteColor()
+
   }
   
   override func viewWillAppear(animated: Bool) {
     handleNetworkError()
     
-    self.navigationController?.navigationBarHidden = false
+    self.navigationController?.navigationBarHidden = true
+  }
+  
+  override func viewWillDisappear(animated: Bool) {
+    handleNetworkError()
+    
+        self.navigationController?.navigationBarHidden = false
   }
   
   override func didReceiveMemoryWarning() {
