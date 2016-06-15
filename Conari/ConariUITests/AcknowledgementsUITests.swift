fcb7679e37010573c97b0c695074e4c5ab06e342
Paul Krassnig <Paul@tug-swl-f132.tu-graz.ac.at>
2016-06-15 13:47:42 +0200

    [PK / PP] fixed some uiTest errors

--- Conari/ConariUITests/AcknowledgementsUITests.swift
+++ Conari/ConariUITests/AcknowledgementsUITests.swift
@@ -30,8 +30,11 @@ class AcknowledgementsUITests: XCTestCase {
 
     func testExample() {
       
+      sleep(1)
       let app = XCUIApplication()
+      sleep(1)
       app.buttons["About"].tap()
+      sleep(1)
       app.buttons["Acknowledgements"].tap()
       
       let _ = app.otherElements["Open Source Licenses"].childrenMatchingType(.Other).elementBoundByIndex(6).staticTexts["Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:"]
